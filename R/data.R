ae786c4abbb0852e2b91e38efbe3abd69a16a355
fionarhuang <fionarhuang@gmail.com>
2019-06-17 11:35:13 +0200

    add data

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' A selected microbial data on human saliva bodysite.
+#' 
+#' A TreeSummarizedExperiment object stores the seleted (top abundant) 1000 OTU
+#' from the human microbial data on saliva site from 226 samples.
+#'
+#' 
+#'
+"saliva"
