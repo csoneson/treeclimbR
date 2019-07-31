9db4cc3c132c75179bd9f829a49c9019e8f5f1ed
fionarhuang <fionarhuang@gmail.com>
2019-07-31 17:03:37 +0200

    scoreTree: rename argument

--- R/scoreTree.R
+++ R/scoreTree.R
@@ -8,14 +8,13 @@
 #' update those in higher level of the tree until the root is reached.
 #' 
 #' @param tree A phylo object
-#' @param scoreData A data frame that includes at least two columns. One column
+#' @param score_data A data frame that includes at least two columns. One column
 #'   stores the number of the node, and the other stores the original score of
 #'   the corresponding node.
-#' @param nodeCol The name of the column that stores the number of the node.
-#' @param scoreCol The name of the column that stores the original score of the
+#' @param node_column The name of the column that stores the number of the node.
+#' @param score_column The name of the column that stores the original score of the
 #'   node.
-#' @param FUN A function used to generate the score at each internal node, e.g.
-#'   sum.
+#' @param new_score The name of the column to store the new generated score.
 #' 
 #' @importFrom TreeSummarizedExperiment printNode matTree
 #' @export
@@ -37,10 +36,10 @@
 #'
 #' # update the score based on the family provided by the tree
 #' newScore <- scoreTree(tree = tinyTree, 
-#'                       scoreData = exScore,
-#'                       nodeCol = "nodeNum", 
-#'                       scoreCol = "score",
-#'                       FUN = mean)
+#'                       score_data = exScore,
+#'                       node_column = "nodeNum", 
+#'                       score_column = "score",
+#'                       new_score = "treeScore")
 #'
 #' # visualize the result
 #' df <- newScore %>% 
@@ -56,9 +55,8 @@
 
 
 
-scoreTree <- function(tree, scoreData, nodeCol, 
-                      scoreCol, FUN) {
-    
+scoreTree <- function(tree, score_data, node_column,
+                      score_column, new_score) {
     df <- printNode(tree, type = "all")
     nodeA <- sort(df$nodeNum)
     tip <- sort(df$nodeNum[df$isLeaf])
@@ -79,8 +77,8 @@ scoreTree <- function(tree, scoreData, nodeCol,
     
     # nodes at different levels
     mat <- matTree(tree = tree)
-    tempData <- scoreData
-    tempData$treeScore <- tempData[, scoreCol]
+    tempData <- score_data
+    tempData[[new_score]] <- tempData[, score_column]
     
     # rule out leaves
     tempMat <- mat[, -1]
@@ -94,10 +92,10 @@ scoreTree <- function(tree, scoreData, nodeCol,
         # (a-b; a-c) => scoreNA = mean(scoreA, scoreB, scoreC) 
         score.i <- lapply(node.i, FUN = function(x){
             desd.i <- desdA[[x]]
-            rr.i <- match(desd.i, tempData[, nodeCol])
-            temp.i <- tempData[rr.i, "treeScore"]
+            rr.i <- match(desd.i, tempData[, node_column])
+            temp.i <- tempData[rr.i, new_score]
             temp.i <- cbind(temp.i[!is.na(temp.i)])
-            auc <- apply(temp.i, 2, FUN = FUN)
+            auc <- apply(temp.i, 2, FUN = mean)
             return(auc)
         })
         # aucSE.i <- lapply(node.i, FUN = function(x){
@@ -108,11 +106,70 @@ scoreTree <- function(tree, scoreData, nodeCol,
         #     return(newSE)
         # })
         # 
-        row.i <- match(node.i, tempData[, nodeCol])
-        tempData[row.i, "treeScore"] <- unlist(score.i)
+        row.i <- match(node.i, tempData[, node_column])
+        tempData[row.i, new_score] <- unlist(score.i)
         #tempData[row.i, "aucSE"] <- unlist(aucSE.i)
     }
     
     return(tempData)
-    
 }
+# function(tree, score_data, node_column, 
+#                       score_column, FUN) {
+#     
+#     df <- printNode(tree, type = "all")
+#     nodeA <- sort(df$nodeNum)
+#     tip <- sort(df$nodeNum[df$isLeaf])
+#     nodeI <- sort(setdiff(nodeA, tip))
+#     
+#     if (!all.equal(nodeA, c(tip, nodeI))) {
+#         warnings("The nodeNum should start from leaves with number 1.")
+#     }
+#     
+#     
+#     desd <- lapply(nodeI, FUN = function(x) {
+#         xx <- findChild(tree = tree, node = x)
+#         out <- c(x, xx)
+#         return(out)
+#     })
+#     
+#     desdA <- c(as.list(tip), desd)
+#     
+#     # nodes at different levels
+#     mat <- matTree(tree = tree)
+#     tempData <- score_data
+#     tempData$treeScore <- tempData[, score_column]
+#     
+#     # rule out leaves
+#     tempMat <- mat[, -1]
+#     for (i in seq_len(ncol(tempMat))) {
+#         mat.i <- tempMat[, i]
+#         mat.o <- tempMat[, -seq_len(i)]
+#         node.i <- setdiff(as.vector(mat.i), as.vector(mat.o))
+#         node.i <- node.i[!is.na(node.i)]
+#         
+#         # take the average score of the whole family
+#         # (a-b; a-c) => scoreNA = mean(scoreA, scoreB, scoreC) 
+#         score.i <- lapply(node.i, FUN = function(x){
+#             desd.i <- desdA[[x]]
+#             rr.i <- match(desd.i, tempData[, node_column])
+#             temp.i <- tempData[rr.i, "treeScore"]
+#             temp.i <- cbind(temp.i[!is.na(temp.i)])
+#             auc <- apply(temp.i, 2, FUN = FUN)
+#             return(auc)
+#         })
+#         # aucSE.i <- lapply(node.i, FUN = function(x){
+#         #     desd.i <- desdA[[x]]
+#         #     rr.i <- match(desd.i, tempData[, "nodeNum"])
+#         #     aucSE <- tempData[rr.i, "aucSE"]
+#         #     newSE <- sqrt(sum(aucSE^2))
+#         #     return(newSE)
+#         # })
+#         # 
+#         row.i <- match(node.i, tempData[, node_column])
+#         tempData[row.i, "treeScore"] <- unlist(score.i)
+#         #tempData[row.i, "aucSE"] <- unlist(aucSE.i)
+#     }
+#     
+#     return(tempData)
+#     
+# }
