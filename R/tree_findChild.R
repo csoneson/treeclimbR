7e232c06c83470699892af9b0998d250dfd1ce2e
fionarhuang <ruizhuRH@gmail.com>
2019-10-08 09:37:57 +0200

    findChild: improve the speed

--- R/tree_findChild.R
+++ R/tree_findChild.R
@@ -27,32 +27,79 @@
 #' geom_text2(aes(label = label), color = "darkorange",
 #'            hjust = -0.1, vjust = -0.7)
 #'
-#' (tips <- findChild(tree = tinyTree, node = 17))
+#' (tips <- findChild(tree = tinyTree, 
+#'                    node = c(17, 12)))
 #' 
 
 
 
-findChild <- function(tree, node = 11, use.alias = FALSE) {
+findChild <- function(tree, node,
+                      use.alias = FALSE) {
     
-    if (!is(tree, "phylo")) {
-        stop("tree requires a phylo object. \n")
+    if (!inherits(tree, "phylo")) {
+        stop("tree: should be a phylo object")
     }
     
-    if (is.factor(node)) {
-        stop("node requires character or numeric.")
+    if (!(is.character(node) |
+          is.numeric(node) |
+          is.integer(node))) {
+        stop("The argument (node) should be character or numeric")
     }
+    # the edge matrix
+    mat <- tree$edge
+    matN <- matTree(tree = tree)
     
     if (is.character(node)) {
-        node <- transNode(tree = tree, node = node)
+        numA <- transNode(tree = tree, node = node,
+                          use.alias = TRUE,
+                          message = FALSE)
+    } else {
+        numA <- node
+        isOut <- !numA %in% mat
+        if (any(isOut)) {
+            stop("Node ", numA,
+                 " can't be found in the ",
+                 deparse(substitute(tree)), "\n")
+        }
+        
     }
     
-    mat <- tree$edge
-    ind <- which(mat == node, arr.ind = TRUE)
-    ind <- ind[ind[,"col"] == 1, , drop = FALSE]
+    # find children
+    loc1 <- lapply(numA, FUN = function(x) {
+        xi <- which(matN == x, arr.ind = TRUE)
+        return(xi)
+    })
     
-    ind[, "col"] <- ind[, "col"] + 1
-    child <- mat[ind]
-    names(child) <- transNode(tree = tree, node = child,
-                              use.alias = use.alias, message = FALSE)
-    return(child)
+    loc2 <- lapply(loc1, FUN = function(x) {
+        x1 <- x[, "row", drop = FALSE]
+        x2 <- x[, "col", drop = FALSE] - 1
+        cbind(row = x1, col = x2)
+        
+    })
+    
+    matNN <- apply(matN, 2, FUN = function(x) {
+        xe <- x[!is.na(x)]
+        xx <- transNode(tree = tree, node = xe,
+                        use.alias = use.alias,
+                        message = FALSE)
+        x[!is.na(x)] <- xx
+        return(x)
+    })
+    
+    
+    # descendants: tips or leaves
+    tipA <- unique(setdiff(mat[, 2], mat[, 1]))
+    
+    chl <- lapply(seq_along(loc2), FUN = function(x) {
+        xx <- loc2[[x]]
+        y <- matN[xx]
+        uy <- unique(y)
+        return(uy)
+    })
+    
+    # final output (node number or label)
+    names(chl) <- transNode(tree = tree, node = numA,
+                            use.alias = use.alias,
+                            message = FALSE)
+    return(chl)
 }
