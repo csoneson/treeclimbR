576f2a9a9d6b6a35607eba4107e805525b5b0549
Charlotte Soneson <charlottesoneson@gmail.com>
2024-01-30 11:25:32 +0100

    Update/expand documentation of infoCand

--- R/infoCand.R
+++ R/infoCand.R
@@ -1,15 +1,19 @@
 #' Get information of candidates
-#' 
-#' \code{infoCand} extract information about candidates 
-#' 
-#' @param object the output of the function \link{evalCand}.
-#' @export
-#' @return a data frame
+#'
+#' Extract information about candidates.
+#'
 #' @author Ruizhu Huang
+#' @export
+#'
+#' @param object The output of \link{evalCand()} (a list).
+#'
+#' @return A \code{data.frame} with information about candidates.
+#'
 #' @examples
 #' library(TreeSummarizedExperiment)
 #' library(ggtree)
 #'
+#' ## Simulate some data
 #' data(tinyTree)
 #' ggtree(tinyTree, branch.length = "none") +
 #'    geom_text2(aes(label = node)) +
@@ -17,37 +21,46 @@
 #'    geom_hilight(node = 18, fill = "orange", alpha = 0.5)
 #' set.seed(2)
 #' pv <- runif(19, 0, 1)
-#' pv[c(1:5, 13, 14, 18)] <- runif(8, 0, 0.001)
+#' pv[c(seq_len(5), 13, 14, 18)] <- runif(8, 0, 0.001)
 #'
 #' fc <- sample(c(-1, 1), 19, replace = TRUE)
-#' fc[c(1:3, 13, 14)] <- 1
+#' fc[c(seq_len(3), 13, 14)] <- 1
 #' fc[c(4, 5, 18)] <- -1
-#' df <- data.frame(node = 1:19,
+#' df <- data.frame(node = seq_len(19),
 #'                  pvalue = pv,
 #'                  foldChange = fc)
+#'
+#' ## Get candidates
 #' ll <- getCand(tree = tinyTree, score_data = df,
-#'               #t = seq(0, 1, by = 0.05),
 #'                node_column = "node",
 #'                p_column = "pvalue",
 #'                sign_column = "foldChange")
+#'
+#' ## Evaluate candidates
 #' cc <- evalCand(tree = tinyTree, levels = ll$candidate_list,
 #'                score_data = df, node_column = "node",
 #'                p_column = "pvalue", sign_column = "foldChange",
 #'                limit_rej = 0.05)
-#'                
+#'
+#' ## Get summary info about candidates
 #' out <- infoCand(object = cc)
+#' out
+#'
+infoCand <- function(object) {
+    .assertVector(x = object, type = "list")
 
-infoCand <- function(object){
     info <- object$level_info
-    
+
     if (is.null(info)) {
-        stop("obj should be the output from 'evalCand'")
-    } 
-    
-    isNA <- all(is.na(info$rej_pseudo_leaf))
-    if (isNA) {
+        stop("object needs to have a 'level_info' slot - make sure that you ",
+             "are using output from 'evalCand()'")
+    }
+
+    ## Remove rej_pseudo_leaf and rej_pseudo_node columns if all values are NA
+    allNA <- all(is.na(info$rej_pseudo_leaf))
+    if (allNA) {
         info <- subset(info, select = -c(rej_pseudo_leaf, rej_pseudo_node))
     }
-  
+
     return(info)
 }
