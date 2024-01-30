e7cf279dd1afda67829fd6dfbb61af7aabf80a95
Charlotte Soneson <charlottesoneson@gmail.com>
2024-01-30 10:53:13 +0100

    Update/expand documentation for aggDS

--- R/aggDS.R
+++ R/aggDS.R
@@ -1,109 +1,161 @@
-#' aggregate data
-#' \code{aggDS} aggregate data based on the tree.
+#' Aggregate observed data based on a tree
 #'
-#' @param TSE A \code{TreeSummarizedExperiment} object
-#' @param assay A name or a numeric value to index a matrix in \code{assays}.
-#' @param sample_id The column name of sample id.
-#' @param group_id The column name of group id.
-#' @param cluster_id The column name of clusters.
-#' @param FUN The function
-#' @param message TRUE or FALSE. If TRUE, the running process is shown.
+#' Aggregate observed values based on a column (sample) tree, e.g. for
+#' differential state analysis. The returned object will contain one
+#' abundance matrix for each node in the tree, with columns corresponding
+#' to sample IDs and rows corresponding to the same features as the rows of
+#' the input object. The nodes correspond to
+#' either the original cell clusters, or larger metaclusters encompassing
+#' several of the original clusters (defined by the provided column tree).
 #'
-#' @importFrom SummarizedExperiment assays colData
-#' @import TreeSummarizedExperiment
-#' @importFrom dplyr "%>%" select mutate
-#' @importFrom tibble column_to_rownames
-#' @importFrom S4Vectors t split
+#' @author Ruizhu Huang, Charlotte Soneson
 #' @export
 #'
-#' @return A singleCellExperiment object. In \code{assays}, each matrix is
-#'     data of a node on the tree. Rows of matrix represent genes and columns
-#'     represent samples.
+#' @param TSE A \code{TreeSummarizedExperiment} object. Rows represent
+#'     variables (e.g., genes) and columns represent observations (e.g., cells).
+#'     The object must contain a column tree, whose tips represent initial
+#'     cell clusters (the \code{cluster_id} annotation indicates which of
+#'     these clusters a cell belongs to). The internal nodes represent
+#'     increasingly coarse partitions of the cells obtained by successively
+#'     merging the original clusters according to the provided column tree.
+#' @param assay The name of the assay from \code{TSE} to aggregate values from.
+#' @param sample_id A character scalar indicating the column of
+#'     \code{colData(TSE)} that corresponds to the sample ID. These will be the
+#'     columns of the output object.
+#' @param group_id A character scalar indicating the column of
+#'     \code{colData(TSE)} that corresponds to the group/condition. This
+#'     information will be propagated to the aggregated object.
+#' @param cluster_id A character scalar indicating the column of
+#'     \code{colData(TSE)} that corresponds to the initial cluster ID for
+#'     each cell.
+#' @param FUN The aggregation function.
+#' @param message A logical value, indicating whether progress messages
+#'     should be printed to the console.
+#'
+#' @importFrom SummarizedExperiment SummarizedExperiment colData assays assayNames
+#' @importFrom TreeSummarizedExperiment findDescendant convertNode showNode colTree
+#' @importFrom dplyr select distinct mutate
+#'
+#' @return A \code{SummarizedExperiment} object. Each assay represents the
+#'     aggregated values for one node in the tree.
 #'
 #' @examples
 #' library(treeclimbR)
 #' library(ape)
 #'
-#' tr <- rtree(3, tip.label = LETTERS[1:3])
-#' cc <- matrix(rpois(50, 10), nrow = 5)
-#' rownames(cc) <- paste0("gene", 1:5)
-#' colnames(cc) <- paste0("cell", 1:10)
-#' cd <- data.frame(sid = rep(1:2, each = 5),
-#'                  gid = rep(letters[1:2], each = 5),
-#'                  cid = sample(LETTERS[1:3], size = 10, replace = TRUE),
+#' set.seed(1L)
+#' tr <- rtree(3, tip.label = LETTERS[seq_len(3)])
+#' plot(tr)
+#' cc <- matrix(rpois(60, 10), nrow = 6)
+#' rownames(cc) <- paste0("gene", seq_len(6))
+#' colnames(cc) <- paste0("cell", seq_len(10))
+#' cd <- data.frame(sid = rep(seq_len(2), each = 5),
+#'                  gid = rep(letters[seq_len(2)], each = 5),
+#'                  cid = sample(LETTERS[seq_len(3)], size = 10, replace = TRUE),
 #'                  stringsAsFactors = FALSE)
-#' tse <- TreeSummarizedExperiment(assays = list(cc),
+#' tse <- TreeSummarizedExperiment(assays = list(counts = cc),
 #'                                 colTree = tr,
 #'                                 colNodeLab = cd$cid,
 #'                                 colData = cd)
 #'
-#' out <- aggDS(TSE = tse, assay = 1, sample_id = "sid",
+#' out <- aggDS(TSE = tse, assay = "counts", sample_id = "sid",
 #'              group_id = "gid", cluster_id = "cid")
-
-aggDS <- function(TSE,
-                  assay = "counts",
-                  sample_id = "sample_id",
-                  group_id = "group_id",
-                  cluster_id = "cluster_id",
+#'
+#' ## Aggregated counts for the node alias_4
+#' SummarizedExperiment::assay(out, "alias_4")
+#'
+aggDS <- function(TSE, assay = "counts", sample_id = "sample_id",
+                  group_id = "group_id", cluster_id = "cluster_id",
                   FUN = sum, message = FALSE) {
 
-    if (message) {
-        message("Preparing ...") }
+    ## Check arguments
+    ## -------------------------------------------------------------------------
+    .assertVector(x = TSE, type = "TreeSummarizedExperiment")
+    .assertScalar(x = assay, type = "character")
+    stopifnot(assay %in% SummarizedExperiment::assayNames(TSE))
+    .assertScalar(x = sample_id, type = "character")
+    .assertScalar(x = group_id, type = "character")
+    .assertScalar(x = cluster_id, type = "character")
+    stopifnot(all(c(sample_id, group_id, cluster_id) %in%
+                      colnames(SummarizedExperiment::colData(TSE))))
+    .assertScalar(x = message, type = "logical")
+    .assertVector(x = FUN, type = "function")
+    .assertVector(x = TreeSummarizedExperiment::colTree(TSE), type = "phylo")
+    stopifnot(all(SummarizedExperiment::colData(TSE)[[cluster_id]] %in%
+                      TreeSummarizedExperiment::colTree(TSE)$tip.label))
 
-    # cell information
+    ## Cell information
+    ## -------------------------------------------------------------------------
     if (message) {
-        message("Extracting cell information ...") }
-    tree <- colTree(TSE)
-    cell_info <- colData(TSE)[, c(sample_id, group_id, cluster_id)]
+        message("Extracting cell information ...")
+    }
+    tree <- TreeSummarizedExperiment::colTree(TSE)
+    cell_info <- SummarizedExperiment::colData(TSE)[, c(sample_id, group_id,
+                                                        cluster_id)]
     colnames(cell_info) <- c("sample_id", "group_id", "cluster_id")
-    cell_info$node <- convertNode(tree = tree,
-                                node = as.character(cell_info$cluster_id))
+    ## Find the node number for each original cluster and add to cell info
+    cell_info$node <- TreeSummarizedExperiment::convertNode(
+        tree = tree, node = as.character(cell_info$cluster_id))
 
-    # tree information
+    ## Tree information
+    ## -------------------------------------------------------------------------
     if (message) {
-        message("Extracting tree information ...") }
-    node <- showNode(tree = tree, only.leaf = FALSE, use.alias = TRUE)
-    desd_list <- findDescendant(tree = tree, node = node,
-                        only.leaf = TRUE, self.include = TRUE)
+        message("Extracting tree information ...")
+    }
+    ## Get node number corresponding to each alias
+    node <- TreeSummarizedExperiment::showNode(tree = tree, only.leaf = FALSE,
+                                               use.alias = TRUE)
+    ## Find descendant leaves (node numbers) for each node
+    desd_list <- TreeSummarizedExperiment::findDescendant(
+        tree = tree, node = node, only.leaf = TRUE, self.include = TRUE)
     names(desd_list) <- names(node)
 
-    ## indicators
+    ## Indicators
+    ## -------------------------------------------------------------------------
     if (message) {
-        message("Grouping cells by samples and nodes ...") }
-    # an indicator: cells belong to each node of the tree
+        message("Grouping cells by samples and nodes ...")
+    }
+    ## For each node, find the cells belonging to it (included in one of the
+    ## corresponding leaf clusters)
     ind_list1 <- lapply(desd_list, FUN = function(x) {
         which(cell_info$node %in% x)
     })
 
-    # an indicator: cells belong to each sample
+    ## For each sample, find the cells belonging to it
     sp <- unique(cell_info$sample_id)
     ind_list2 <- lapply(sp, FUN = function(x) {
         which(cell_info$sample_id %in% x)
     })
     names(ind_list2) <- sp
 
-    # an indicator: cells to each combination of nodes and samples
+    ## For each node, construct a list where each element gives the cells
+    ## corresponding to that node and a given sample
     ind_list3 <- lapply(ind_list1, FUN = function(x) {
         xx <- lapply(ind_list2, intersect, y = x)
     })
 
-
     if (message) {
-        message("Preparing data on each node ...") }
-    ## reshape data
-    asy <- assays(TSE)[[assay]]
+        message("Preparing data on each node ...")
+    }
+    ## Reshape data
+    ## -------------------------------------------------------------------------
+    asy <- SummarizedExperiment::assays(TSE)[[assay]]
+    ## Initialize the list that will hold assays (one for each node)
     dat_list <- vector("list", length(ind_list3))
     names(dat_list) <- names(ind_list3)
+    ## Populate each node assay
     for (i in seq_along(dat_list)) {
         ind_i <- ind_list3[[i]]
         sp_i <- names(ind_i)
         asy_i <- lapply(ind_i, FUN = function(x) {
-            # genes in rows, cells in columns
+            ## Get assay values for node and sample
+            ## Genes in rows, cells in columns
             xx <- asy[, x, drop = FALSE]
 
-            # aggregation
+            ## Aggregate
             ax <- apply(xx, 1, FUN = FUN)
         })
+        ## Assemble aggregated matrix
         asy_i <- do.call(cbind, asy_i)
         colnames(asy_i) <- sp_i
         dat_list[[i]] <- asy_i
@@ -115,45 +167,50 @@ aggDS <- function(TSE,
         }
     }
 
-    # sample information
+    ## Sample information
+    ## -------------------------------------------------------------------------
     if (message) {
-        message("Working on sample information ...") }
-    sample_df <- colData(TSE)[, c(sample_id, group_id)] %>%
-        data.frame() %>%
+        message("Working on sample information ...")
+    }
+    sample_df <- SummarizedExperiment::colData(TSE)[, c(sample_id, group_id)] |>
+        data.frame() |>
         distinct()
     sample_info <- sample_df[, group_id, drop = FALSE]
     rownames(sample_info) <- sample_df[[sample_id]]
-    sample_info <- sample_info[colnames(dat_list[[1]]), ,drop = FALSE]
+    sample_info <- sample_info[colnames(dat_list[[1]]), , drop = FALSE]
 
-    # meta data:
+    ## Metadata
+    ## -------------------------------------------------------------------------
     if (message) {
-        message("Working on metadata ...") }
+        message("Working on metadata ...")
+    }
     cell_n <- table(cell_info[["sample_id"]])
-    experiment_info <- cell_info %>%
-        data.frame() %>%
-        select(sample_id, group_id) %>%
-        distinct() %>%
+    experiment_info <- cell_info |>
+        data.frame() |>
+        select(sample_id, group_id) |>
+        distinct() |>
         mutate(n_cells = cell_n[sample_id])
 
     agg_pars <- list(assay = assay,
                      by = c(cluster_id, sample_id),
                      fun = deparse(substitute(FUN)),
                      scale = FALSE)
-    # on leaf level
+
+    ## On leaf level
     cell_tab <- table(cell_info[["cluster_id"]],
                       cell_info[["sample_id"]])
-    rn <- convertNode(tree = tree,
-                    node = rownames(cell_tab))
+    rn <- TreeSummarizedExperiment::convertNode(tree = tree,
+                                                node = rownames(cell_tab))
 
-    # on all levels
-    desd <- findDescendant(tree = tree, node = node,
-                   only.leaf = TRUE, self.include = TRUE)
+    ## On all levels
+    ## 'node' has the node number for each alias
+    desd <- TreeSummarizedExperiment::findDescendant(
+        tree = tree, node = node, only.leaf = TRUE, self.include = TRUE)
     cell_tab <- as.matrix(cell_tab)
     clus_tab <- lapply(desd, FUN = function(x) {
         ii <- match(x, rn)
         xi <- cell_tab[ii, , drop = FALSE]
         apply(xi, 2, sum)
-
     })
     clus_tab <- do.call(rbind, clus_tab)
     rownames(clus_tab) <- names(node)
@@ -161,14 +218,14 @@ aggDS <- function(TSE,
 
     meta <- list(experiment_info = experiment_info,
                  agg_pars = agg_pars,
-                 n_cells = clus_tab )
-    # output
+                 n_cells = clus_tab)
+
+    ## Output
     if (message) {
-        message("Output data ...") }
-    out <- SummarizedExperiment(assays = dat_list,
-                                colData = sample_info,
-                                metadata = meta)
+        message("Output data ...")
+    }
+    out <- SummarizedExperiment::SummarizedExperiment(assays = dat_list,
+                                                      colData = sample_info,
+                                                      metadata = meta)
     return(out)
-
-
 }
