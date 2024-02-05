79956849725653ee17e23ea1843a0d49c86d7fed
Charlotte Soneson <charlottesoneson@gmail.com>
2024-02-05 17:20:45 +0100

    Update documentation for fdr() and tpr()

--- R/fdr.R
+++ R/fdr.R
@@ -1,134 +1,136 @@
 #' Calculate false discovery rate (FDR) on a tree structure
 #'
-#' \code{FDR} calculates the false discovery rate on a tree structure at leaf or
+#' Calculate the false discovery rate on a tree structure, at either leaf or
 #' node level.
 #'
-#' @param tree A phylo object
-#' @param truth Nodes that have signals (eg. differentally abundant at different
-#'   experimental conditions.). \strong{Note:} when TPR is required at leaf
-#'   level (\code{only.leaf = TRUE}), the descendant leaves of the given nodes
-#'   will be found out and the TPR is calculated on the leaf level;
-#' @param found Nodes that have been found to have signal (eg. differentally
-#'   abundant at different experimental conditions). \strong{Note:} when TPR is
-#'   required at leaf level (\code{only.leaf = TRUE}), the descendant leaves of
-#'   the given nodes will be found out and the TPR is calculated on the leaf
-#'   level;
-#' @param only.leaf A logical value, TRUE or FALSE. If TRUE, true positive rate
-#'   is calculated at the leaf (tip) level; otherwise it is calculated at node
-#'   level. The default is TRUE
-#'   
-#' @export
-#' @return A true positive rate
 #' @author Ruizhu Huang
+#' @export
+#'
+#' @param tree A \code{phylo} object.
+#' @param truth True signal nodes (e.g., nodes that are truly differentially
+#'     abundant between experimental conditions). \strong{Note:} when the
+#'     FDR is requested at the leaf level (\code{only.leaf = TRUE}), the
+#'     descendant leaves of the given nodes will be found and the FDR will be
+#'     estimated on the leaf level.
+#' @param found Detected signal nodes (e.g., nodes that have been found to be
+#'     differentially abundant via a statistical testing procedure).
+#'     \strong{Note:} when the FDR is requested at the leaf level
+#'     (\code{only.leaf = TRUE}), the descendant leaves of the given nodes will
+#'     be found out and the FDR will be estimated on the leaf level.
+#' @param only.leaf A logical scalar. If \code{TRUE}, the false discovery rate
+#'     is calculated at the leaf (tip) level; otherwise it is calculated at
+#'     the node level.
+#'
+#' @return The estimated false discovery rate.
+#'
+#' @importFrom TreeSummarizedExperiment convertNode
+#'
 #' @examples
-#' library(ggtree)
-#' library(TreeSummarizedExperiment)
+#' suppressPackageStartupMessages({
+#'     library(ggtree)
+#'     library(TreeSummarizedExperiment)
+#' })
+#'
 #' data("tinyTree")
-#' ggtree(tinyTree) + 
-#'    geom_text2(aes(label = node)) + 
+#'
+#' ## Two branches are truly differential
+#' ggtree(tinyTree) +
+#'    geom_text2(aes(label = node)) +
 #'    geom_hilight(node = 16, fill = "orange", alpha = 0.3) +
 #'    geom_hilight(node = 13, fill = "blue", alpha = 0.3)
 #'
-#' # Truth: two branches have differential
-#' # abundance under different conditions.
+#' ## FDR at the leaf level (1/8)
+#' fdr(tree = tinyTree, truth = c(16, 13),
+#'     found = c(15, 14), only.leaf = TRUE)
 #'
-#' fdr1 <- fdr(tree = tinyTree, truth = c(16, 13),
-#'             found = c(17, 14), only.leaf = TRUE)
-#'  # fdr at the node level
-#' fdr2 <- fdr(tree = tinyTree, truth = c(16, 13),
-#'             found = c("Node_15", "Node_14"), only.leaf = FALSE)
+#' ## FDR at the node level (2/14)
+#' fdr(tree = tinyTree, truth = c(16, 13),
+#'     found = c(15, 14), only.leaf = FALSE)
 #'
-#'
+fdr <- function(tree, truth, found, only.leaf = TRUE) {
+    .assertVector(x = tree, type = "phylo")
+    .assertScalar(x = only.leaf, type = "logical")
 
-
-fdr <- function(tree, truth, found,
-                only.leaf = TRUE) {
-    
-    if (!inherits(tree, "phylo")) {
-        stop("tree: should be a phylo object")
-    }
-    
     if (is.character(truth)) {
-        truth <- convertNode(tree = tree, node = truth,
-                           message = FALSE)
+        truth <- TreeSummarizedExperiment::convertNode(
+            tree = tree, node = truth, message = FALSE)
     }
     if (is.character(found)) {
-        found <- convertNode(tree = tree, node = found,
-                           message = FALSE)
+        found <- TreeSummarizedExperiment::convertNode(
+            tree = tree, node = found, message = FALSE)
     }
-    
+
     tt <- .fdr0(tree = tree, truth = truth,
                 found = found, only.leaf = only.leaf)
     fdr <- tt[1]/tt[2]
-    
-    
-    # return final results
     names(fdr) <- "fdr"
+
     return(fdr)
 }
 
 
-#' Calculate true positives and positives
+#' Count false positives and all positives
 #'
-#' \code{.fdr0} calculates the number of false discovery and the total number of
-#' discovery on a tree structure at leaf or node level.
+#' Calculate the number of false discoveries and the total
+#' number of discoveries on a tree structure, at leaf or node level.
 #'
-#' @param tree A phylo object
-#' @param truth Nodes that have signals (eg. differentally abundant at different
-#'  experimental conditions.).
-#' @param found Nodes that have been found to have signal
-#' @param only.leaf A logical value, TRUE or FALSE. If TRUE, true positive rate
-#'   is calculated at the leaf (tip) level; otherwise it is calculated at node
-#'   level. The default is TRUE
-#' @return a vector
+#' @author Ruizhu Huang
+#'
+#' @param tree A \code{phylo} object.
+#' @param truth True signal nodes (e.g., nodes that are truly differentially
+#'     abundant between experimental conditions).
+#' @param found Detected signal nodes (e.g., nodes that have been found to be
+#'     differentially abundant via a statistical testing procedure).
+#' @param only.leaf A logical scalar. If \code{TRUE}, the false discovery rate
+#'     is calculated at the leaf (tip) level; otherwise it is calculated at
+#'     the node level.
+#'
+#' @return A vector with two numbers, the number of false discoveries and the
+#' total number of discoveries.
+#'
 #' @keywords internal
+#' @noRd
+#'
+#' @importFrom TreeSummarizedExperiment findDescendant
+#'
+.fdr0 <- function(tree, truth = NULL, found = NULL, only.leaf = TRUE) {
 
+    ## Check inputs
+    ## -------------------------------------------------------------------------
+    if (!is.null(truth) && !(is.character(truth) || is.numeric(truth))) {
+        stop("'truth' should be either a character vector or a numeric",
+             "vector")
+    }
 
-.fdr0 <- function(tree,
-                  truth = NULL,
-                  found = NULL,
-                  only.leaf = TRUE) {
-    
-    # ================= check inputs =======================
-    if (!is.null(truth)) {
-        # if diff exists, check whether the truth has correct input format
-        if (!(is.character(truth) |
-              is.numeric(truth) |
-              is.integer(truth))) {
-            stop("truth should include character or numeric")
-        }
+    if (!is.null(found) && !(is.character(found) || is.numeric(found))) {
+        stop("'found' should be either a character vector or a numeric",
+             "vector")
     }
-    
-    if (!is.null(found)) {
-        # check whether 'found' has correct input
-        if (!(is.character(found) |
-              is.numeric(found) |
-              is.integer(found))) {
-            stop("found should include character or numeric")
-        } 
-    }
-    
-    # ================= without discovery =======================
-    if (is.null(found) | length(found) == 0) {
-        c(fd = 0,  disc = 1)
+
+    ## Count discoveries
+    ## -------------------------------------------------------------------------
+    if (is.null(found) || length(found) == 0) {
+        ## Nothing is found
+        c(fd = 0, disc = 1)
     } else {
-        
-        # =================   with discovery  =======================
-        nodeF <- findDescendant(tree = tree, node = found,
-                        only.leaf = only.leaf, self.include = TRUE)
+        ## Something is found
+        nodeF <- TreeSummarizedExperiment::findDescendant(
+            tree = tree, node = found, only.leaf = only.leaf,
+            self.include = TRUE)
         nodeF <- unique(unlist(nodeF))
-        
-        # if no diff
+
         if (is.null(truth)) {
-            c(fd = length(nodeF), disc = length(nodeF)) 
+            ## No truly differential nodes
+            c(fd = length(nodeF), disc = length(nodeF))
         } else {
-           # if has diff
-           nodeT <- findDescendant(tree = tree, node = truth,
-                           only.leaf = only.leaf, self.include = TRUE)
-           nodeT <- unique(unlist(nodeT))
-           # false discovery & discovery
-           fd <- setdiff(nodeF, nodeT)
-           c(fd = length(fd), disc = length(nodeF))
+            ## Some truly differential nodes
+            nodeT <- TreeSummarizedExperiment::findDescendant(
+                tree = tree, node = truth, only.leaf = only.leaf,
+                self.include = TRUE)
+            nodeT <- unique(unlist(nodeT))
+
+            fd <- setdiff(nodeF, nodeT)
+            c(fd = length(fd), disc = length(nodeF))
         }
     }
 }
