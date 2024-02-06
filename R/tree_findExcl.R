e791a64440ce35d8aaa5b272e9b216ccc81cfd0c
Charlotte Soneson <charlottesoneson@gmail.com>
2024-02-06 09:52:04 +0100

    Small changes to documentation of findExcl

--- R/tree_findExcl.R
+++ R/tree_findExcl.R
@@ -1,56 +1,71 @@
-#' find branches that are exclusive from the specified branches
-#' 
-#' \code{findExcl} is to find all branches that have leaves exclusive from the
-#' specified branches. In the same time, these branches group the leaves to have
-#' the min number of branches. Examples are given in example section to better
-#' clarify this.
-#' 
-#' @param tree A phylo object.
-#' @param node A numeric or charater vector specifying the nodes.
-#' @param use.alias A logical value, TRUE or FALSE. If TRUE, the alias name is
-#'   used to name the output vector.
+#' Find branches that are non-overlapping with specified branches in a tree
+#'
+#' Find all branches whose leaves do not overlap with those of the specified
+#' branches.
+#'
+#' @author Ruizhu Huang
 #' @export
-#' @examples 
+#'
+#' @param tree A \code{phylo} object.
+#' @param node A numeric or character vector specifying the nodes.
+#' @param use.alias A logical scalar. If \code{TRUE}, the alias name is
+#'     used to name the output vector.
+#'
+#' @importFrom TreeSummarizedExperiment convertNode
+#'
+#' @examples
 #' library(ggtree)
 #' data(tinyTree)
 #' ggtree(tinyTree, branch.length = "none") +
-#' geom_text2(aes(label = node)) +
-#' geom_hilight(node = 17, fill = "blue", alpha = 0.3) +
-#' geom_hilight(node = 13, fill = "orange", alpha = 0.3)
-#' 
-#' # To output branches that have leaves not in the two colored branches, the
-#' # shortest result here is three. They can't be grouped as less branches
-#' # without including the leaves in the specified branches.
+#'     geom_text2(aes(label = node)) +
+#'     geom_hilight(node = 17, fill = "blue", alpha = 0.3) +
+#'     geom_hilight(node = 13, fill = "orange", alpha = 0.3)
+#'
+#' ## Find branches whose leaves do not overlap with the two colored branches.
+#' ## The returned branches are represented at the highest tree level
+#' ## possible without including any of the forbidden branches.
 #' findExcl(tree = tinyTree, node = c(17, 13))
-
-
-findExcl <- function(tree, node,
-                     use.alias = FALSE){
-    
-    if (is.character(node)) {
-        node <- convertNode(tree = tree, node = node,
-                           message = FALSE)
-    } else {
-        node <- node
+#'
+findExcl <- function(tree, node, use.alias = FALSE) {
+    ## Check input arguments
+    ## -------------------------------------------------------------------------
+    .assertVector(x = tree, type = "phylo")
+    .assertScalar(x = use.alias, type = "logical")
+    if (!(is.character(node) || is.numeric(node))) {
+        stop("'node' should be either a character vector or a numeric ",
+             "vector")
     }
-    
-    # find descendant leaves of node
-    inT <- findDescendant(tree = tree, node = node, only.leaf = TRUE)
+
+    ## Convert node labels to node number
+    ## -------------------------------------------------------------------------
+    if (is.character(node)) {
+        node <- TreeSummarizedExperiment::convertNode(
+            tree = tree, node = node, message = FALSE)
+    }
+
+    ## Find descendant leaves of nodes
+    ## -------------------------------------------------------------------------
+    inT <- TreeSummarizedExperiment::findDescendant(
+        tree = tree, node = node, only.leaf = TRUE)
     inT <- unlist(inT)
-    # find all leaves of tree
-    allT <- setdiff(tree$edge[,2], tree$edge[, 1])
-    
-    # Leaves not included in node
+
+    ## Get all leaves in the tree
+    ## -------------------------------------------------------------------------
+    allT <- setdiff(tree$edge[, 2], tree$edge[, 1])
+
+    ## Leaves that are not descendants of node
+    ## -------------------------------------------------------------------------
     exT <- setdiff(allT, inT)
-    
-    # replace leaves with their ancestor branch node
-    out <- joinNode(tree = tree, node = exT)
-    
-    # return a vector of the found node (the node number of the node)
-    # name the vector with the node label
-    names(out) <- convertNode(tree = tree, node = out,
-                            use.alias = use.alias,
-                            message = FALSE)
+
+    ## Replace leaves with their ancestor branch node
+    ## -------------------------------------------------------------------------
+    out <- TreeSummarizedExperiment::joinNode(tree = tree, node = exT)
+
+    ## Return a vector of the found node (the node number of the node)
+    ## Name the vector with the node label
+    ## -------------------------------------------------------------------------
+    names(out) <- TreeSummarizedExperiment::convertNode(
+        tree = tree, node = out, use.alias = use.alias, message = FALSE)
+
     return(out)
-    
 }
