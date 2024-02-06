1a43d6f71ef0d8bf691f88f244f634a94a4c8803
Charlotte Soneson <charlottesoneson@gmail.com>
2024-02-06 09:33:16 +0100

    Simplify findChild and expand documentation

--- R/tree_findChild.R
+++ R/tree_findChild.R
@@ -1,105 +1,101 @@
-#' Find the children
+#' Find the children of an internal node in a tree
 #'
-#' \code{findChild} finds children of an internal node.
+#' Find the direct children of an internal node in a tree.
 #'
-#' @param node An internal node. It could be the node number or the node
-#'   label.
-#' @param tree A phylo object.
-#' @param use.alias A logical value, TRUE or FALSE. The default is FALSE, and
-#'   the node label would be used to name the output; otherwise, the alias of
-#'   node label would be used to name the output. The alias of node label is
-#'   created by adding a prefix \code{"alia_"} to the node number.
+#' @author Ruizhu Huang, Charlotte Soneson
 #' @export
+#'
+#' @param node Either the node number or node label of an internal node of
+#'     \code{tree}.
+#' @param tree A \code{phylo} object.
+#' @param use.alias A logical scalar. If \code{FALSE} (default), the node label
+#'     is used to name the output; otherwise, the alias of the node label is
+#'     used. The alias of the node label is created by adding a prefix
+#'     \code{"alias_"} to the node number.
+#'
 #' @return A vector of nodes. The numeric value is the node number, and the
-#'   vector name is the corresponding node label. If a node has no label, it
-#'   would have NA as name when \code{use.alias = FALSE}, and have the alias of
-#'   node label as name when \code{use.alias = TRUE}.
-#' @author Ruizhu Huang
+#'     vector name is the corresponding node label. If a node has no label, it
+#'     would have NA as name when \code{use.alias = FALSE}, and have the alias
+#'     of the node label as name when \code{use.alias = TRUE}.
+#'
+#' @importFrom TreeSummarizedExperiment matTree convertNode
 #'
 #' @examples
-#' data(tinyTree)
-#'
 #' library(ggtree)
-#' ggtree(tinyTree) +
-#' geom_text2(aes(label = node), color = "darkblue",
-#'                hjust = -0.5, vjust = 0.7) +
-#' geom_hilight(node = 17, fill = 'steelblue', alpha = 0.5) +
-#' geom_text2(aes(label = label), color = "darkorange",
-#'            hjust = -0.1, vjust = -0.7)
 #'
-#' (tips <- findChild(tree = tinyTree, 
-#'                    node = c(17, 12)))
-#' 
-
-
-
-findChild <- function(tree, node,
-                      use.alias = FALSE) {
-    
-    if (!inherits(tree, "phylo")) {
-        stop("tree: should be a phylo object")
+#' data(tinyTree)
+#' ggtree(tinyTree) +
+#'     geom_text2(aes(label = node), color = "darkblue",
+#'                hjust = -0.5, vjust = 0.7) +
+#'     geom_hilight(node = 17, fill = 'steelblue', alpha = 0.5) +
+#'     geom_text2(aes(label = label), color = "darkorange",
+#'                hjust = -0.1, vjust = -0.7)
+#'
+#' ## Specify node numbers
+#' findChild(tree = tinyTree, node = c(17, 12))
+#'
+#' ## Name return values using aliases
+#' findChild(tree = tinyTree, node = c(17, 12), use.alias = TRUE)
+#'
+#' ## Specify node labels
+#' findChild(tree = tinyTree, node = c("Node_17", "Node_12"))
+#'
+#' ## Tips have no children
+#' findChild(tree = tinyTree, node = "t4")
+#'
+findChild <- function(tree, node, use.alias = FALSE) {
+    .assertVector(x = tree, type = "phylo")
+    .assertScalar(x = use.alias, type = "logical")
+    if (!(is.character(node) || is.numeric(node))) {
+        stop("'node' should be either a character vector or a numeric ",
+             "vector")
     }
-    
-    if (!(is.character(node) |
-          is.numeric(node) |
-          is.integer(node))) {
-        stop("The argument (node) should be character or numeric")
-    }
-    # the edge matrix
+
+    ## Get the edge matrix and the path from each node to the root
+    ## -------------------------------------------------------------------------
     mat <- tree$edge
-    matN <- matTree(tree = tree)
-    
+    matN <- TreeSummarizedExperiment::matTree(tree = tree)
+
+    ## Get node number and check that it is part of the tree
+    ## -------------------------------------------------------------------------
     if (is.character(node)) {
-        numA <- convertNode(tree = tree, node = node,
-                          use.alias = TRUE,
-                          message = FALSE)
+        numA <- TreeSummarizedExperiment::convertNode(
+            tree = tree, node = node, use.alias = TRUE, message = FALSE)
     } else {
         numA <- node
         isOut <- !numA %in% mat
         if (any(isOut)) {
-            stop("Node ", numA,
-                 " can't be found in the ",
+            stop("Node ", numA, " can't be found in ",
                  deparse(substitute(tree)), "\n")
         }
-        
     }
-    
-    # find children
+
+    ## Find positions of each node in matN
+    ## -------------------------------------------------------------------------
     loc1 <- lapply(numA, FUN = function(x) {
-        xi <- which(matN == x, arr.ind = TRUE)
-        return(xi)
+        which(matN == x, arr.ind = TRUE)
     })
-    
+
+    ## Find positions of children in matN
+    ## -------------------------------------------------------------------------
     loc2 <- lapply(loc1, FUN = function(x) {
         x1 <- x[, "row", drop = FALSE]
         x2 <- x[, "col", drop = FALSE] - 1
         cbind(row = x1, col = x2)
-        
     })
-    
-    matNN <- apply(matN, 2, FUN = function(x) {
-        xe <- x[!is.na(x)]
-        xx <- convertNode(tree = tree, node = xe,
-                        use.alias = use.alias,
-                        message = FALSE)
-        x[!is.na(x)] <- xx
-        return(x)
-    })
-    
-    
-    # descendants: tips or leaves
-    tipA <- unique(setdiff(mat[, 2], mat[, 1]))
-    
+
+    ## Get node numbers for children
+    ## -------------------------------------------------------------------------
     chl <- lapply(seq_along(loc2), FUN = function(x) {
         xx <- loc2[[x]]
         y <- matN[xx]
-        uy <- unique(y)
-        return(uy)
+        return(unique(y))
     })
-    
-    # final output (node number or label)
-    names(chl) <- convertNode(tree = tree, node = numA,
-                            use.alias = use.alias,
-                            message = FALSE)
+
+    ## Final output (node number or label)
+    ## -------------------------------------------------------------------------
+    names(chl) <- TreeSummarizedExperiment::convertNode(
+        tree = tree, node = numA, use.alias = use.alias, message = FALSE)
+
     return(chl)
 }
