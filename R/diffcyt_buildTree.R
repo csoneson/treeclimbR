e796cbe99893c6f75098299021c06bd110eefcdb
Charlotte Soneson <charlottesoneson@gmail.com>
2024-02-06 17:41:59 +0100

    Consolidate documentation for diffcyt workflow

--- R/diffcyt_buildTree.R
+++ R/diffcyt_buildTree.R
@@ -1,29 +1,5 @@
-#' Build a tree from diffcyt high-resolution cell clusters
-#'
-#' This is a part of the suite of functions that can be applied downstream of
-#' a \code{diffcyt} analysis.
-#' Apply hierarchical clustering to build a tree starting from a high-resolution
-#' clustering created by the \code{\link[diffcyt]{generateClusters}} function
-#' from the \code{diffcyt} package. The function calculates the median
-#' abundance for each (ID type) marker and cluster, and uses this data to
-#' further aggregate the initial clusters using hierarchical clustering.
-#'
-#' @author Ruizhu Huang
 #' @export
-#'
-#' @param d_se A \code{SummarizedExperiment} object, with cells as rows and
-#'     features as columns. This should be the output
-#'     from \code{\link[diffcyt]{generateClusters}}. The \code{colData} is
-#'     assumed to contain a factor named \code{marker_class}.
-#' @param dist_method The distance measure to be used. This must be one of
-#'     "euclidean", "maximum", "manhattan", "canberra", "binary" or "minkowski".
-#'     Any unambiguous substring can be given. Please refer to \code{method} in
-#'     \code{\link[stats]{dist}} for more information.
-#' @param hclust_method The agglomeration method to be used. This should be (an
-#'     unambiguous abbreviation of) one of "ward.D", "ward.D2", "single",
-#'     "complete", "average" (= UPGMA), "mcquitty" (= WPGMA), "median" (= WPGMC)
-#'     or "centroid" (= UPGMC). Please refer to \code{method} in
-#'     \code{\link[stats]{hclust}} for more information.
+#' @rdname diffcyt_workflow
 #'
 #' @importFrom diffcyt calcMediansByClusterMarker
 #' @importFrom stats hclust dist
@@ -31,52 +7,6 @@
 #' @importFrom SummarizedExperiment assay
 #' @importFrom TreeSummarizedExperiment addLabel
 #' @importFrom S4Vectors metadata
-#'
-#' @return A \code{phylo} object representing the hierarchical clustering of
-#' the initial high-resolution clusters.
-#'
-#' @examples
-#' # For a complete workflow example demonstrating each step in the 'diffcyt'
-#' # pipeline, please see the diffcyt vignette.
-#' library(diffcyt)
-#'
-#' # Function to create random data (one sample)
-#' d_random <- function(n = 20000, mean = 0, sd = 1, ncol = 20, cofactor = 5) {
-#'     d <- sinh(matrix(rnorm(n, mean, sd), ncol = ncol)) * cofactor
-#'     colnames(d) <- paste0("marker", sprintf("%02d", 1:ncol))
-#'     d
-#' }
-#'
-#' # Create random data (without differential signal)
-#' set.seed(123)
-#' d_input <- list(
-#'     sample1 = d_random(), sample2 = d_random(),
-#'     sample3 = d_random(), sample4 = d_random()
-#' )
-#'
-#' experiment_info <- data.frame(
-#'     sample_id = factor(paste0("sample", seq_len(4))),
-#'     group_id = factor(c("group1", "group1", "group2", "group2"))
-#' )
-#'
-#' marker_info <- data.frame(
-#'     channel_name = paste0("channel", sprintf("%03d", seq_len(20))),
-#'     marker_name = paste0("marker", sprintf("%02d", seq_len(20))),
-#'     marker_class = factor(c(rep("type", 10), rep("state", 10)),
-#'                           levels = c("type", "state", "none"))
-#' )
-#'
-#' # Prepare data
-#' d_se <- prepareData(d_input, experiment_info, marker_info)
-#'
-#' # Transform data
-#' d_se <- transformData(d_se)
-#'
-#' # Generate clusters
-#' d_se <- generateClusters(d_se)
-#'
-#' # Build a tree
-#' tr <- buildTree(d_se)
 #'
 buildTree <- function(d_se, dist_method = "euclidean",
                       hclust_method = "average") {
@@ -92,7 +22,7 @@ buildTree <- function(d_se, dist_method = "euclidean",
     d_medians <- diffcyt::calcMediansByClusterMarker(d_se)
     md <- SummarizedExperiment::assay(d_medians)[, S4Vectors::metadata(
         d_medians)$id_type_markers]
-    tree_h <- stats::hclust(dist(md, method = dist_method),
+    tree_h <- stats::hclust(d = stats::dist(md, method = dist_method),
                             method = hclust_method)
 
     tree_p <- ape::as.phylo(tree_h)
--- R/diffcyt_calcTreeMedians.R
+++ R/diffcyt_calcTreeMedians.R
@@ -1,129 +1,55 @@
-#' Calculate cluster medians on the tree structure \code{calcTreeMedians}
-#' calculates cluster medians (median expression for each cluster-sample-marker
-#' combination). This is a tree version of \code{\link[diffcyt]{calcMedians}}.
-#' The clusters used in \code{\link[diffcyt]{calcMedians}} are clusters on the
-#' leaf level of the tree here. More details about the data could be found in
-#' \code{\link[diffcyt]{calcMedians}}.
-#' 
-#' @param d_se Data object from previous steps, in
-#'   \code{\link[TreeSummarizedExperiment]{TreeSummarizedExperiment}} format,
-#'   containing cluster labels as a column in the row meta-data (from
-#'   \code{\link[diffcyt]{generateClusters}}). Column meta-data is assumed to
-#'   contain a factor marker_class.
-#' @param tree A phylo object from \code{\link{buildTree}}
-#' @param message A logic value, TRUE or FALSE
-#' @return A TreeSummarizedExperiment object.  Calculate median marker
-#'   expression for each cluster (each node of the tree) and sample (i.e.
-#'   medians for each cluster-sample-marker combination).
+#' @export
+#' @rdname diffcyt_workflow
 #'
-#'   The data object is assumed to contain a factor \code{marker_class} in the
-#'   column meta-data (see \code{\link{prepareData}}), which indicates the
-#'   protein marker class for each column of data (\code{"type"},
-#'   \code{"state"}, or \code{"none"}).
-#'
-#'   The cluster medians are required for testing for differential states within
-#'   cell populations, and for plotting purposes.
-#'
-#'   Variables \code{id_type_markers} and \code{id_state_markers} are saved in
-#'   the \code{metadata} slot of the output object. These can be used to
-#'   identify the 'cell type' and 'cell state' markers in the list of
-#'   \code{assays} in the output
-#'   \code{\link[TreeSummarizedExperiment]{TreeSummarizedExperiment}} object,
-#'   which is useful in later steps of the 'diffcyt' pipeline.
-#'
-#'   Results are returned as a new
-#'   \code{\link[TreeSummarizedExperiment]{TreeSummarizedExperiment}} object,
-#'   where rows = clusters, columns = samples, sheets (\code{assays} slot) =
-#'   markers. Note that there is a separate table of values (\code{assay}) for
-#'   each marker. The \code{metadata} slot also contains variables
-#'   \code{id_type_markers} and \code{id_state_markers}, which can be used to
-#'   identify the sets of cell type and cell state markers in the list of
-#'   \code{assays}.
-#' @import TreeSummarizedExperiment
+#' @importFrom TreeSummarizedExperiment TreeSummarizedExperiment convertNode
+#'     aggTSE rowTree
 #' @importFrom dplyr select distinct
 #' @importFrom methods is
 #' @importFrom stats median
-#' @export
-#' @return A TreeSummarizedExperiment object
-#' @examples 
-#' # For a complete workflow example demonstrating each step, please see the
-#' # vignette of 'diffcyt'
-#' 
-#' library(diffcyt)
-#' 
-#' # Function to create random data (one sample)
-#' d_random <- function(n = 20000, mean = 0, sd = 1, ncol = 20, cofactor = 5) {
-#'     d <- sinh(matrix(rnorm(n, mean, sd), ncol = ncol)) * cofactor
-#'     colnames(d) <- paste0("marker", sprintf("%02d", 1:ncol))
-#'     d
-#' }
-#' 
-#' # Create random data (without differential signal)
-#' set.seed(123)
-#' d_input <- list(
-#'     sample1 = d_random(), 
-#'     sample2 = d_random(), 
-#'     sample3 = d_random(), 
-#'     sample4 = d_random()
-#' )
-#' 
-#' experiment_info <- data.frame(
-#'     sample_id = factor(paste0("sample", 1:4)), 
-#'     group_id = factor(c("group1", "group1", "group2", "group2")), 
-#'     stringsAsFactors = FALSE
-#' )
-#' 
-#' marker_info <- data.frame(
-#'     channel_name = paste0("channel", sprintf("%03d", 1:20)), 
-#'     marker_name = paste0("marker", sprintf("%02d", 1:20)), 
-#'     marker_class = factor(c(rep("type", 10), rep("state", 10)), 
-#'                           levels = c("type", "state", "none")), 
-#'     stringsAsFactors = FALSE
-#' )
-#' 
-#' # Prepare data
-#' d_se <- prepareData(d_input, experiment_info, marker_info)
-#' 
-#' # Transform data
-#' d_se <- transformData(d_se)
-#' 
-#' # Generate clusters
-#' d_se <- generateClusters(d_se)
-#' 
-#' # build a tree
-#' tr <- buildTree(d_se)
-#' 
-#' # calculate medians on nodes of a tree
-#' d_medians_tree <- calcTreeMedians(d_se = d_se, tree = tr)
-
+#' @importFrom SummarizedExperiment rowData assays colData
+#' @importFrom S4Vectors metadata
+#'
 calcTreeMedians <- function(d_se, tree, message = FALSE) {
-    
-    if (!("cluster_id" %in% (colnames(rowData(d_se))))) {
-        stop("Data object does not contain cluster labels. 
+
+    ## Check arguments
+    ## -------------------------------------------------------------------------
+    .assertScalar(x = message, type = "logical")
+    .assertVector(x = d_se, type = "SummarizedExperiment")
+    if (!("cluster_id" %in% (colnames(SummarizedExperiment::rowData(d_se))))) {
+        stop("Data object does not contain cluster labels.
              Run 'diffcyt::generateClusters' to generate cluster labels.")
     }
-    
-    if (!is(tree, "phylo")) {
+    if (!methods::is(tree, "phylo")) {
         stop("tree is not a phylo object.
-             Run 'buildTree(d_se)' to generate the tree")
+             Run 'buildTree(d_se)' to generate the tree.")
     }
-    
-    ## create the tse object
-    rlab <- as.character(rowData(d_se)$cluster_id)
-    d_lse <- TreeSummarizedExperiment(assays = assays(d_se),
-                                      rowData = rowData(d_se),
-                                      rowTree = tree, 
-                                      rowNodeLab = rlab,
-                                      colData = colData(d_se),
-                                      metadata = metadata(d_se))
-    d_lse <- d_lse[, colData(d_lse)$marker_class %in% c("type", "state")]
-    
-    ## split by patient_id
-    rd <- rowData(d_lse)
+
+    ## Create the tse object
+    ## -------------------------------------------------------------------------
+    rlab <- as.character(SummarizedExperiment::rowData(d_se)$cluster_id)
+    d_lse <- TreeSummarizedExperiment::TreeSummarizedExperiment(
+        assays = SummarizedExperiment::assays(d_se),
+        rowData = SummarizedExperiment::rowData(d_se),
+        rowTree = tree,
+        rowNodeLab = rlab,
+        colData = SummarizedExperiment::colData(d_se),
+        metadata = S4Vectors::metadata(d_se))
+
+    ## Retain only type or state markers
+    d_lse <- d_lse[, SummarizedExperiment::colData(d_lse)$marker_class %in%
+                       c("type", "state")]
+    if (ncol(d_lse) == 0) {
+        stop("No type or state markers found in the object.")
+    }
+
+    ## Calculate required summaries
+    ## -------------------------------------------------------------------------
+    rd <- SummarizedExperiment::rowData(d_lse)
     sid <- unique(rd$sample_id)
     nodes <- sort(unique(as.vector(tree$edge)))
-    labs <- convertNode(tree = tree, 
-                        node = nodes, use.alias = TRUE)
+    labs <- TreeSummarizedExperiment::convertNode(
+        tree = tree, node = nodes, use.alias = TRUE)
+
     lc <- lapply(seq_along(sid), FUN = function(i) {
         if (message) {
             message("Working on ", i, " out of ", length(sid), " samples.")
@@ -131,15 +57,16 @@ calcTreeMedians <- function(d_se, tree, message = FALSE) {
         x <- sid[i]
         sel <- rd$sample_id == x
         xx <- d_lse[sel, ]
-        ax <- aggTSE(x = xx, rowLevel = nodes, 
-                       rowFun = function(x){
-                           median(x, na.rm = TRUE)
-                       }, message = message)
-        
-        # counts
-        cx <- assays(ax)[[1]]
-        
-        # for missing nodes
+        ax <- TreeSummarizedExperiment::aggTSE(
+            x = xx, rowLevel = nodes,
+            rowFun = function(x) {
+                stats::median(x, na.rm = TRUE)
+            }, message = message)
+
+        ## Counts
+        cx <- SummarizedExperiment::assays(ax)[[1]]
+
+        ## For missing nodes
         nam <- setdiff(labs, rownames(cx))
         if (length(nam)) {
             mx <- matrix(NA, nrow = length(nam), ncol = ncol(cx))
@@ -148,12 +75,12 @@ calcTreeMedians <- function(d_se, tree, message = FALSE) {
         } else {
             mx <- NULL
         }
-        
+
         cx <- rbind(cx, mx)[labs, ]
         return(cx)
     })
-    
-    ## assays : reform data to split by markers
+
+    ## assays: reshape data to split by markers
     rc <- do.call(rbind, lc)
     lcm <- lapply(seq_len(ncol(rc)), FUN = function(x) {
         xx <- matrix(rc[, x], ncol = length(sid), byrow = FALSE)
@@ -162,32 +89,37 @@ calcTreeMedians <- function(d_se, tree, message = FALSE) {
         return(xx)
     })
     names(lcm) <- colnames(d_lse)
-    
-    # rowdata
+
+    ## Row data
     rd <- cbind.data.frame(cluster_id = factor(labs, levels = labs))
     rownames(rd) <- labs
-    
-    # column data
-    cd <- rowData(d_se) %>%
-        data.frame() %>%
-        select(group_id, sample_id) %>%
-        distinct() 
-    cd <- cd[match(cd$sample_id, sid), ]
-    
-    # metadata
-    tM <- colData(d_lse)$marker_name[colData(d_lse)$marker_class == "type"]
-    sM <- colData(d_lse)$marker_name[colData(d_lse)$marker_class == "state"]
+
+    ## Column data
+    cd <- SummarizedExperiment::rowData(d_se) |>
+        data.frame() |>
+        select(group_id, sample_id) |>
+        distinct()
+    cd <- cd[match(cd$sample_id, sid), , drop = FALSE]
+
+    ## Metadata
+    tM <- SummarizedExperiment::colData(d_lse)$marker_name[
+        SummarizedExperiment::colData(d_lse)$marker_class == "type"]
+    sM <- SummarizedExperiment::colData(d_lse)$marker_name[
+        SummarizedExperiment::colData(d_lse)$marker_class == "state"]
     md <- list(id_type_markers = colnames(d_lse) %in% tM,
                id_state_markers = colnames(d_lse) %in% sM)
-    
-    # row node label
-    rnl <- convertNode(tree = rowTree(d_lse), node = nodes, use.alias = TRUE)
-    # output
-    out <- TreeSummarizedExperiment(assays = lcm,
-                                    rowData  = rd,
-                                    colData = cd,
-                                    metadata = md,
-                                    rowTree = rowTree(d_lse),
-                                    rowNodeLab = rnl)
+
+    ## Row node label
+    rnl <- TreeSummarizedExperiment::convertNode(
+        tree = TreeSummarizedExperiment::rowTree(d_lse), node = nodes,
+        use.alias = TRUE)
+
+    ## Assemble output
+    ## -------------------------------------------------------------------------
+    out <- TreeSummarizedExperiment::TreeSummarizedExperiment(
+        assays = lcm, rowData  = rd, colData = cd, metadata = md,
+        rowTree = TreeSummarizedExperiment::rowTree(d_lse),
+        rowNodeLab = rnl)
+
     return(out)
 }
