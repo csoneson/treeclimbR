fb57570d3146c05621ab556c44e97ee098e90819
Charlotte Soneson <charlottesoneson@gmail.com>
2024-02-06 11:26:58 +0100

    Expand documentation for edgerWrp

--- R/edgerWrp.R
+++ R/edgerWrp.R
@@ -1,105 +1,116 @@
-#' A wrapper function of edgeR
+#' Wrapper applying an edgeR differential analysis workflow
 #'
 #' \code{edgerWrp} is a wrapper using functions from the \code{\link{edgeR}}
 #' (Robinson et al. 2010, \emph{Bioinformatics}; McCarthy et al. 2012,
-#' \emph{Nucleic Acids Research}) to fit models and calculate moderated test for
-#' each entity. We have used \code{\link[edgeR]{estimateDisp}} to estimate the
-#' dispersion. The statistical methods implemented in the \code{edgeR} package
-#' were originally designed for the analysis of gene expression data such as
-#' RNA-sequencing counts. Here, we apply these methods to counts that might be
-#' the abundance of  microbes or cells.
+#' \emph{Nucleic Acids Research}) to fit models and perform a moderated test for
+#' each entity.
 #'
-#' The experimental design must be specified using a design matrix. The
-#' customized design matrix could be given by \code{design}.
+#' The function performs the following steps:
+#' \itemize{
+#' \item Create a \code{\link[edgeR]{DGEList}} object. If \code{lib_size} is
+#' given, set the library sizes to these values, otherwise use the column sums
+#' of the count matrix.
+#' \item If \code{normalize} is \code{TRUE}, estimate normalization factors
+#' using \code{\link[edgeR]{calcNormFactors}}.
+#' \item Estimate dispersions with \code{\link[edgeR]{estimateDisp}}.
+#' \item Depending on the value of \code{option}, apply either the LRT or
+#' QLF edgeR workflows (i.e., either \code{\link[edgeR]{glmFit}} +
+#' \code{\link[edgeR]{glmLRT}} or \code{\link[edgeR]{glmQLFit}} +
+#' \code{\link[edgeR]{glmQLFTest}}), testing for the specified contrast.
+#' }
 #'
-#' Normalization for samples is automatically performed by \code{edgeR} package.
-#' More details about the calculation of normalization factor could be found
-#' from \code{\link[edgeR]{calcNormFactors}}.
-#'
-#' @param count A matrix with features (e.g., genes or microbes) on rows and
-#'   samples on columns.
-#' @param lib_size A numeric vector to specify the library size. If NULL
-#'   (default), the column sum of \code{count} is used.
-#' @param option  \code{"glm"} or \code{"glmQL"}. If \code{"glm"},
-#'   \code{\link[edgeR]{glmFit}} and \code{\link[edgeR]{glmLRT}} are used;
-#'   otherwise, \code{\link[edgeR]{glmQLFit}} and
-#'   \code{\link[edgeR]{glmQLFTest}} are used. Details about the difference
-#'   between two options are in the help page of \code{\link[edgeR]{glmQLFit}}.
-#' @param design A numeric matrix. It must be of full column rank created by
-#'   \code{\link[stats]{model.matrix}}. Please refer to \code{design} in
-#'   \code{\link[edgeR]{glmQLFit}} and \code{\link[edgeR]{glmFit}}
-#' @param contrast A numeric vector specifying one contrast of
-#'   the linear model coefficients to be tested equal to zero. Its length
-#'   must equal to the number of columns of design. If NULL, the last
-#'   coefficient will be tested equal to zero. Please refer to \code{contrast}
-#'   in \code{\link[edgeR]{glmQLFTest}} and \code{\link[edgeR]{glmLRT}}.
-#' @param normalize A logical value, TRUE or FALSE, to specify whether
-#'   \code{count} should be normalized. The default is TRUE.
-#' @param normalize_method Normalization method to be used. Please refer to
-#'   \code{method} in \code{\link[edgeR]{calcNormFactors}} for more details.
-#' @param ... More arguments to pass to \code{\link[edgeR]{glmFit}}
-#'   (\code{option = "glm"} or \code{\link[edgeR]{glmQLFit}} (\code{option =
-#'   "glmQL"}).
-#' @importFrom edgeR DGEList calcNormFactors estimateDisp glmFit glmLRT
-#'   glmQLFit glmQLFTest
+#' @author Ruizhu Huang
 #' @export
+#'
+#' @param count A matrix with features (e.g., genes or microbes) in rows and
+#'     samples in columns.
+#' @param lib_size A numeric vector with library sizes for each sample. If
+#'     \code{NULL} (default), the column sums of \code{count} are used.
+#' @param option Either \code{"glm"} or \code{"glmQL"}. If \code{"glm"},
+#'     \code{\link[edgeR]{glmFit}} and \code{\link[edgeR]{glmLRT}} are used;
+#'     otherwise, \code{\link[edgeR]{glmQLFit}} and
+#'     \code{\link[edgeR]{glmQLFTest}} are used. Details about the difference
+#'     between the two options can be found in the help pages of
+#'     \code{\link[edgeR]{glmQLFit}}.
+#' @param design A numeric design matrix, e.g. created by
+#'     \code{\link[stats]{model.matrix}}. Please refer to \code{design} in
+#'     \code{\link[edgeR]{glmQLFit}} and \code{\link[edgeR]{glmFit}} for
+#'     more details.
+#' @param contrast A numeric vector specifying one contrast of
+#'     the linear model coefficients to be tested. Its length
+#'     must equal the number of columns of \code{design}. If \code{NULL}, the
+#'     last coefficient will be tested. Please refer to \code{contrast}
+#'     in \code{\link[edgeR]{glmQLFTest}} and \code{\link[edgeR]{glmLRT}} for
+#'     more details.
+#' @param normalize A logical scalar, specifying whether normalization factors
+#'     should be calculated (using \code{\link[edgeR]{calcNormFactors}}).
+#' @param normalize_method Normalization method to be used. Please refer to
+#'     \code{method} in \code{\link[edgeR]{calcNormFactors}} for more details.
+#' @param ... More arguments to pass to \code{\link[edgeR]{glmFit}}
+#'     (if \code{option = "glm"} or \code{\link[edgeR]{glmQLFit}}
+#'     (if \code{option = "glmQL"}).
+#'
+#' @importFrom edgeR DGEList calcNormFactors estimateDisp glmFit glmLRT
+#'     glmQLFit glmQLFTest
+#'
 #' @return The output of \code{\link[edgeR]{glmQLFTest}} or
-#'   \code{\link[edgeR]{glmLRT}} depends on the specified \code{option}.
+#'     \code{\link[edgeR]{glmLRT}} depending on the specified \code{option}.
+#'
 #' @examples
+#' ## Read example data
+#' x <- readRDS(system.file("extdata/da_sim_100_30_18de.rds",
+#'                          package = "treeclimbR"))
 #'
-#' nlibs <- 6
-#' ngenes <- 100
-#' dispersion.true <- 0.1
+#' ## Run differential abundance analysis
+#' out <- edgerWrp(count = assay(x), option = "glm",
+#'                 design = model.matrix(~ group, data = colData(x)),
+#'                 contrast = c(0, 1))
 #'
-#' # Make first 5 gene respond to covariate x
-#' x <- rep(1:2, each = 3)
-#' design <- model.matrix(~ x)
-#' beta.true <- cbind(Beta1 = 2, Beta2 = c(rep(2, 5), rep(0, ngenes-5)))
-#' mu.true <- 2^(beta.true %*% t(design))
-#'
-#' # Generate count data
-#' y <- rnbinom(ngenes*nlibs, mu = mu.true, size = 1/dispersion.true)
-#' y <- matrix(y, ngenes, nlibs)
-#' colnames(y) <- paste(rep(LETTERS[1:2], each = 3),
-#'                      rep(1:3, 2), sep = "_")
-#' rownames(y) <- paste("gene", 1:ngenes, sep=".")
-#'
-#' # analysis
-#' out <- edgerWrp(count = y, option = "glm",
-#'                 design = design, normalize = FALSE)
-#'
-
-edgerWrp <- function(count, lib_size = NULL ,
-                     option = c("glm", "glmQL"),
-                     design = NULL, contrast = NULL,
-                     normalize = TRUE,
+edgerWrp <- function(count, lib_size = NULL, option = c("glm", "glmQL"),
+                     design, contrast = NULL, normalize = TRUE,
                      normalize_method = "TMM", ...) {
 
-    # if the library size isn't given, the column sum is used
+    ## Check input arguments
+    ## -------------------------------------------------------------------------
+    .assertVector(x = count, type = "matrix")
+    .assertVector(x = lib_size, type = "numeric", allowNULL = TRUE,
+                  len = ncol(count))
+    .assertVector(x = design, type = "matrix")
+    .assertVector(x = contrast, type = "numeric", allowNULL = TRUE,
+                  len = ncol(design))
+    .assertScalar(x = normalize, type = "logical")
+    .assertScalar(x = normalize_method, type = "character")
+
+    ## If the library size isn't given, the column sum is used
+    ## -------------------------------------------------------------------------
     if (is.null(lib_size)) {
         lib_size <- apply(count, 2, sum)
     }
 
-    # create the DGEList
-    y <- DGEList(count, remove.zeros = FALSE)
+    ## Create the DGEList
+    ## -------------------------------------------------------------------------
+    y <- edgeR::DGEList(count, remove.zeros = FALSE)
     y$samples$lib.size <- lib_size
 
-    # normalisation
+    ## Normalization
+    ## -------------------------------------------------------------------------
     if (normalize) {
-        y <- calcNormFactors(y, method = normalize_method)
+        y <- edgeR::calcNormFactors(y, method = normalize_method)
     }
 
-    # estimate dispersion
-    y <- estimateDisp(y, design = design)
+    ## Estimate dispersion
+    ## -------------------------------------------------------------------------
+    y <- edgeR::estimateDisp(y, design = design)
 
-    # model and test contrast
+    ## Fit model and test contrast
+    ## -------------------------------------------------------------------------
     option <- match.arg(option)
     if (option == "glm") {
-        fit <- glmFit(y, design = design)
-        lrt <- glmLRT(fit, contrast = contrast)
+        fit <- edgeR::glmFit(y, design = design)
+        lrt <- edgeR::glmLRT(fit, contrast = contrast)
     } else {
-        fit <- glmQLFit(y, design = design)
-        lrt <- glmQLFTest(fit, contrast = contrast)
+        fit <- edgeR::glmQLFit(y, design = design)
+        lrt <- edgeR::glmQLFTest(fit, contrast = contrast)
     }
 
     return(lrt)
