db2a77a8586fe420afd6e95a70603d2996b735fc
Charlotte Soneson <charlottesoneson@gmail.com>
2024-02-14 11:06:34 +0100

    Expand tests simData

--- tests/testthat/test-simData.R
+++ tests/testthat/test-simData.R
@@ -86,6 +86,10 @@ test_that("simData works", {
     args$minTip.A <- c(1, 2)
     expect_error(do.call(simData, args),
                  "'minTip.A' must have length 1")
+    args$minTip.A <- 5
+    args$maxTip.A <- 3
+    expect_error(do.call(simData, args),
+                 "No nodes fulfill the requirements")
 
     args <- args0
     args$minTip.B <- "x"
@@ -219,6 +223,16 @@ test_that("simData works", {
     expect_s3_class(S4Vectors::metadata(out)$branch, "data.frame")
     expect_equal(S4Vectors::metadata(out)$branch$A, 14)
     expect_equal(S4Vectors::metadata(out)$branch$B, 15)
+    ## Calculate proportions for each entity in each sample, average within
+    ## group, check that it agrees reasonably with simulated FCs
+    v <- sweep(assay(out, "counts"), 2, colSums(assay(out, "counts")), "/")
+    expect_gt(cor(rowMeans(v[, 51:100]) / rowMeans(v[, 1:50]),
+                  S4Vectors::metadata(out)$FC), 0.97)
+    expect_lt(sum(abs(rowMeans(v[, 51:100]) / rowMeans(v[, 1:50]) -
+                          metadata(out)$FC)), 2.2)
+    expect_equal(colSums(SummarizedExperiment::assay(out, "counts")),
+                 rep(10000, ncol(out)),
+                 ignore_attr = TRUE)
 
     ## Let function choose branches - with restrictions
     set.seed(1)
@@ -226,8 +240,8 @@ test_that("simData works", {
                    scenario = "BS", from.A = NULL, from.B = NULL,
                    minTip.A = 1, maxTip.A = 5, minTip.B = 2, maxTip.B = 6,
                    minPr.A = 0.1, maxPr.A = 0.9, ratio = 2, adjB = NULL,
-                   pct = 0.6, nSam = c(50, 50), mu = 10000, size = NULL,
-                   n = 1, FUN = sum, message = FALSE)
+                   pct = 0.6, nSam = c(50, 50), mu = c(9900, 10000),
+                   size = NULL, n = 1, FUN = sum, message = FALSE)
     expect_s4_class(out, "TreeSummarizedExperiment")
     expect_equal(nrow(out), nrow(toy_lse))
     expect_equal(ncol(out), 100)
@@ -241,6 +255,13 @@ test_that("simData works", {
     expect_s3_class(S4Vectors::metadata(out)$branch, "data.frame")
     expect_equal(S4Vectors::metadata(out)$branch$A, 13)
     expect_equal(S4Vectors::metadata(out)$branch$B, 15)
+    ## Calculate proportions for each entity in each sample, average within
+    ## group, check that it agrees reasonably with simulated FCs
+    v <- sweep(assay(out, "counts"), 2, colSums(assay(out, "counts")), "/")
+    expect_gt(cor(rowMeans(v[, 51:100]) / rowMeans(v[, 1:50]),
+                  S4Vectors::metadata(out)$FC), 0.97)
+    expect_equal(unique(colSums(SummarizedExperiment::assay(out, "counts"))),
+                 c(10000, 9900))
 
     ## Fix branches - numbers
     set.seed(1)
@@ -248,7 +269,7 @@ test_that("simData works", {
                    scenario = "BS", from.A = 18, from.B = 19,
                    minTip.A = 0, maxTip.A = Inf, minTip.B = 0, maxTip.B = Inf,
                    minPr.A = 0, maxPr.A = 1, ratio = 4, adjB = NULL,
-                   pct = 0.6, nSam = c(50, 50), mu = 10000, size = NULL,
+                   pct = 0.6, nSam = c(50, 50), mu = 10000, size = 100,
                    n = 1, FUN = sum, message = FALSE)
     expect_s4_class(out, "TreeSummarizedExperiment")
     expect_equal(nrow(out), nrow(toy_lse))
@@ -265,6 +286,9 @@ test_that("simData works", {
     expect_equal(S4Vectors::metadata(out)$branch$B, 19)
     expect_true(round(S4Vectors::metadata(out)$branch$ratio, 5) %in%
                     round(S4Vectors::metadata(out)$FC, 5))
+    ## Average library size should be approximately 10000
+    expect_lt(abs(mean(colSums(SummarizedExperiment::assay(out, "counts"))) -
+                      10000), 20)
 
     ## Fix branch A - numbers
     set.seed(1)
@@ -289,6 +313,13 @@ test_that("simData works", {
     expect_equal(S4Vectors::metadata(out)$branch$B, 13)
     expect_true(round(S4Vectors::metadata(out)$branch$ratio, 5) %in%
                     round(S4Vectors::metadata(out)$FC, 5))
+    ## Calculate proportions for each entity in each sample, average within
+    ## group, check that it agrees reasonably with simulated FCs
+    v <- sweep(assay(out, "counts"), 2, colSums(assay(out, "counts")), "/")
+    expect_gt(cor(rowMeans(v[, 51:100]) / rowMeans(v[, 1:50]),
+                  S4Vectors::metadata(out)$FC), 0.89)
+    expect_equal(unique(colSums(SummarizedExperiment::assay(out, "counts"))),
+                 10000)
 
     ## Fix branches - node labels
     set.seed(1)
@@ -383,6 +414,17 @@ test_that("simData works", {
     expect_s3_class(S4Vectors::metadata(out)$branch, "data.frame")
     expect_equal(S4Vectors::metadata(out)$branch$A, "alias_18")
     expect_equal(S4Vectors::metadata(out)$branch$B, "alias_19")
+    ## Calculate proportions for each entity in each sample, average within
+    ## group, check that it agrees reasonably with simulated FCs
+    v <- sweep(assay(out, "counts"), 2, colSums(assay(out, "counts")), "/")
+    expect_gt(cor(rowMeans(v[, 51:100]) / rowMeans(v[, 1:50]),
+                  S4Vectors::metadata(out)$FC), 0.7)
+    expect_equal(unique(colSums(SummarizedExperiment::assay(out, "counts"))),
+                 10000)
+    expect_true(all(S4Vectors::metadata(out)$FC[c("t9", "t4")] < 1))
+    expect_true(all(S4Vectors::metadata(out)$FC[c("t10", "t1")] > 1))
+    expect_false(S4Vectors::metadata(out)$FC["t9"] ==
+                     S4Vectors::metadata(out)$FC["t4"])
 
     ## Scenario US; switch branches
     set.seed(1)
@@ -404,6 +446,17 @@ test_that("simData works", {
     expect_s3_class(S4Vectors::metadata(out)$branch, "data.frame")
     expect_equal(S4Vectors::metadata(out)$branch$A, "alias_19")
     expect_equal(S4Vectors::metadata(out)$branch$B, "alias_18")
+    ## Calculate proportions for each entity in each sample, average within
+    ## group, check that it agrees reasonably with simulated FCs
+    v <- sweep(assay(out, "counts"), 2, colSums(assay(out, "counts")), "/")
+    expect_gt(cor(rowMeans(v[, 51:100]) / rowMeans(v[, 1:50]),
+                  S4Vectors::metadata(out)$FC), 0.7)
+    expect_equal(unique(colSums(SummarizedExperiment::assay(out, "counts"))),
+                 10000)
+    expect_true(all(S4Vectors::metadata(out)$FC[c("t9", "t4")] < 1))
+    expect_true(all(S4Vectors::metadata(out)$FC[c("t10", "t1")] > 1))
+    expect_false(S4Vectors::metadata(out)$FC["t9"] ==
+                     S4Vectors::metadata(out)$FC["t4"])
 
     ## Scenario SS; switch branches
     set.seed(1)
@@ -425,6 +478,8 @@ test_that("simData works", {
     expect_s3_class(S4Vectors::metadata(out)$branch, "data.frame")
     expect_equal(S4Vectors::metadata(out)$branch$A, "alias_19")
     expect_equal(S4Vectors::metadata(out)$branch$B, "alias_18")
+    expect_equal(unique(colSums(SummarizedExperiment::assay(out, "counts"))),
+                 10000)
 
     ## Scenario SS; switch branches - specify size
     set.seed(1)
@@ -447,6 +502,27 @@ test_that("simData works", {
     expect_equal(S4Vectors::metadata(out)$branch$A, "alias_19")
     expect_equal(S4Vectors::metadata(out)$branch$B, "alias_18")
 
+    ## Scenario SS; different branches - specify size
+    set.seed(1)
+    out <- simData(tree = NULL, data = NULL, obj = toy_lse, assay = "counts",
+                   scenario = "SS", from.A = "Node_13", from.B = "Node_15",
+                   minTip.A = 0, maxTip.A = Inf, minTip.B = 0, maxTip.B = Inf,
+                   minPr.A = 0, maxPr.A = 1, ratio = 4, adjB = NULL,
+                   pct = 0.6, nSam = c(50, 50), mu = 10000, size = 0.5,
+                   n = 1, FUN = sum, message = FALSE)
+    expect_s4_class(out, "TreeSummarizedExperiment")
+    expect_equal(nrow(out), nrow(toy_lse))
+    expect_equal(ncol(out), 100)
+    expect_equal(SummarizedExperiment::assayNames(out), "counts")
+    expect_equal(TreeSummarizedExperiment::rowTree(out),
+                 TreeSummarizedExperiment::rowTree(toy_lse))
+    expect_equal(rownames(out), rownames(toy_lse))
+    expect_equal(out$group, rep(c("C1", "C2"), each = 50))
+    expect_type(S4Vectors::metadata(out), "list")
+    expect_s3_class(S4Vectors::metadata(out)$branch, "data.frame")
+    expect_equal(S4Vectors::metadata(out)$branch$A, "alias_13")
+    expect_equal(S4Vectors::metadata(out)$branch$B, "alias_15")
+
     ## Scenario SS; set adjB
     set.seed(1)
     out <- simData(tree = NULL, data = NULL, obj = toy_lse, assay = "counts",
@@ -468,6 +544,77 @@ test_that("simData works", {
     expect_equal(S4Vectors::metadata(out)$branch$A, 14)
     expect_equal(S4Vectors::metadata(out)$branch$B, 15)
 
+    ## Test helper functions
+    ## -------------------------------------------------------------------------
+    pars <- S4Vectors::metadata(parEstimate(toy_lse))$assays.par
+    pk <- .pickLoc(tree = rowTree(toy_lse), data = pars,
+                   from.A  = "alias_18", minTip.A = 0,
+                   maxTip.A = Inf, minTip.B = 0,
+                   maxTip.B = 2, minPr.A = 0,
+                   maxPr.A = 1, ratio = 1)
+    expect_s3_class(pk, "data.frame")
+    expect_named(pk, c("A", "B", "ratio", "A_tips", "B_tips", "A_prop", "B_prop"))
+    expect_equal(pk$A, 18)
+    expect_equal(pk$B, 19)
+    expect_equal(pk$A_tips, 2)
+    expect_equal(pk$B_tips, 2)
+    expect_equal(round(pk$B_prop, 4), round(sum(pars$pi[c("t1", "t10")]), 4)) ## 19
+    expect_equal(round(pk$A_prop, 4), round(sum(pars$pi[c("t4", "t9")]), 4))  ## 18
 
+    pk <- .pickLoc(tree = rowTree(toy_lse), data = pars,
+                   from.A  = "alias_18", minTip.A = 0,
+                   maxTip.A = Inf, minTip.B = 3,
+                   maxTip.B = Inf, minPr.A = 0,
+                   maxPr.A = 1, ratio = 1)
+    expect_s3_class(pk, "data.frame")
+    expect_named(pk, c("A", "B", "ratio", "A_tips", "B_tips", "A_prop", "B_prop"))
+    expect_equal(pk$A, 18)
+    expect_equal(pk$B, 13)
+    expect_equal(pk$A_tips, 2)
+    expect_equal(pk$B_tips, 3)
+    expect_equal(round(pk$B_prop, 4), round(sum(pars$pi[c("t2", "t6", "t7")]), 4)) ## 13
+    expect_equal(round(pk$A_prop, 4), round(sum(pars$pi[c("t4", "t9")]), 4))  ## 18
 
+    expect_warning({
+        expect_error({
+            pk <- .pickLoc(tree = rowTree(toy_lse), data = pars,
+                           from.A  = "alias_18", minTip.A = 0,
+                           maxTip.A = Inf, minTip.B = 6,
+                           maxTip.B = Inf, minPr.A = 0,
+                           maxPr.A = 1, ratio = 1)
+        }, "invalid first argument")
+    }, "no non-missing arguments")
+
+    pk <- .pickLoc(tree = rowTree(toy_lse), data = pars,
+                   from.A  = 14, minTip.A = 0,
+                   maxTip.A = Inf, minTip.B = 6,
+                   maxTip.B = Inf, minPr.A = 0,
+                   maxPr.A = 1, ratio = 1)
+    expect_s3_class(pk, "data.frame")
+    expect_named(pk, c("A", "B", "ratio", "A_tips", "B_tips", "A_prop", "B_prop"))
+    expect_equal(pk$A, 14)
+    expect_equal(pk$B, 15)
+    expect_equal(pk$A_tips, 2)
+    expect_equal(pk$B_tips, 6)
+    expect_equal(round(pk$B_prop, 4), round(sum(pars$pi[c("t1", "t4", "t5", "t8", "t9", "t10")]), 4)) ## 15
+    expect_equal(round(pk$A_prop, 4), round(sum(pars$pi[c("t6", "t7")]), 4))  ## 18
+
+    expect_error({
+        pk <- .pickLoc(tree = rowTree(toy_lse), data = pars,
+                       from.A  = "alias_18", minTip.A = 0,
+                       maxTip.A = Inf, minTip.B = 0,
+                       maxTip.B = Inf, minPr.A = 0,
+                       maxPr.A = 1, ratio = 10)
+    }, "Could not find two branches")
+
+    pk <- .infLoc(tree = rowTree(toy_lse), data = pars,
+                  from.A  = 14, from.B = 15)
+    expect_s3_class(pk, "data.frame")
+    expect_named(pk, c("A", "B", "ratio", "A_tips", "B_tips", "A_prop", "B_prop"))
+    expect_equal(pk$A, 14)
+    expect_equal(pk$B, 15)
+    expect_equal(pk$A_tips, 2)
+    expect_equal(pk$B_tips, 6)
+    expect_equal(round(pk$B_prop, 4), round(sum(pars$pi[c("t1", "t4", "t5", "t8", "t9", "t10")]), 4)) ## 15
+    expect_equal(round(pk$A_prop, 4), round(sum(pars$pi[c("t6", "t7")]), 4))  ## 18
 })
