b651e1939a616607c654172e2ca527e3aeb76605
Charlotte Soneson <charlottesoneson@gmail.com>
2024-02-20 21:06:14 +0100

    Add package documentation

---
+++ R/treeclimbR-package.R
@@ -0,0 +1,16 @@
+#' treeclimbR
+#'
+#' The arrangement of hypotheses in a hierarchical structure appears
+#' in many research fields and often indicates different resolutions at which
+#' data can be viewed. This raises the question of which resolution level
+#' the signal should best be interpreted on. treeclimbR provides a flexible
+#' method to select optimal resolution levels (potentially different levels
+#' in different parts of the tree), rather than cutting the tree at an
+#' arbitrary level. treeclimbR uses a tuning parameter to generate candidate
+#' resolutions and from these selects the optimal one.
+#'
+#' @author Ruizhu Huang
+#' @author Charlotte Soneson
+#'
+#' @keywords internal
+"_PACKAGE"
